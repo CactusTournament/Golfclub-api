b771cfbd3d0f660027cae8d0a1254559681946c3
Brandon <BM6234@gmail.com>
2026-04-04 07:58:06 -0230

    Config, and main application class

---
+++ src/test/java/com/example/golfclub/GolfclubApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.golfclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@org.springframework.test.context.ActiveProfiles("test")
+class GolfclubApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
